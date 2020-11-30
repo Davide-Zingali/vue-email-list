7d3ede420db9a1d38052363c435c427cd98803d5
Davide <kalelc351k@gmail.com>
2020-11-30 23:15:12 +0100

    sostituita con arrow functin, aggiunto indice piu' titolo in html

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 // Descrizione:
 // Genera 10 mail tramite api e stampale in una lista
-// API: https://flynn.boolean.careers/exercises/api/random/mail 
+// API: https://flynn.boolean.careers/exercises/api/random/mail
+
+// versione con arrow function, sfruttando il suo this
 var app = new Vue({
     el: '#root',
     data: {
-        email: '',
         arrayEmail: []
     },
     mounted: function() {
-        // salvo la selezione del this, che in questo caso e' quella di vue, ovvero per tutto il mondo vue
-        let self = this;
-
         for (let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(x) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(x => {
             var emailRandom = x.data.response
-            self.email = emailRandom;
-            self.arrayEmail.push(self.email)
+            this.arrayEmail.push(emailRandom)
             });
         }
     }
 })
+
+
+
+// versione senza arrow function, con salvata la posiszione del this
+// var app = new Vue({
+//     el: '#root',
+//     data: {
+//         email: '',
+//         arrayEmail: []
+//     },
+//     mounted: function() {
+//         // salvo la selezione del this, che in questo caso e' quella di vue, ovvero per tutto il mondo vue
+//         let self = this;
+
+//         for (let i = 0; i < 10; i++) {
+//             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(x) {
+//             var emailRandom = x.data.response
+//             self.email = emailRandom;
+//             self.arrayEmail.push(self.email)
+//             });
+//         }
+//     }
+// })
